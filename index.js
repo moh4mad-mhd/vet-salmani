323e69154ec203ed3db35d6932e736369f23acc5
moh4mad <moh4mad.mhd@gmail.com>
2021-08-05 14:01:50 +0430

    cases table

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server, { cors: { origin: "*" } })
+let date = new Date().toLocaleDateString('fa-IR').replace(/([۰-۹])/g, token => String.fromCharCode(token.charCodeAt(0) - 1728))
 
 app.use(express.json())
 app.use(express.static(__dirname))
